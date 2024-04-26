30b137059b19d7059157c09c7ddc9d64e7f2615e
Goesde Rai <goesderai@gmail.com>
2024-04-26 17:57:27 +0800

    belajar override create & write

--- otoko/models/sale.py
+++ otoko/models/sale.py
@@ -8,16 +8,27 @@ class Sale(models.Model):
     name = fields.Char(string='Sale Number', required=True, copy=False, readonly=True, default=lambda self: _('New'))
     remark = fields.Char(string='Remark')
     customer_id = fields.Many2one(string='Customer', comodel_name='res.partner', ondelete='restrict')
-    date = fields.Datetime(string='Transaction Date')
+    date = fields.Datetime(string='Transaction Date', default=fields.Date.today)
     grand_total = fields.Float(string='Grand Total')
     sale_detail_ids = fields.One2many(comodel_name='otoko.sale_detail',inverse_name='sale_id')
 
+    # Override insert
     @api.model
     def create(self, vals):
         if not (vals.get('name') or vals.get('name') == _('New')):
             vals['name'] = self.env['ir.sequence'].next_by_code('otoko_sale_sequence')
             
         return super(Sale, self).create(vals)
+    
+    # Override update
+    def write(self, vals):
+        if not self:
+            return true
+        
+        if 'date' not in vals:
+            vals['date'] = fields.Date.today()
+        
+        return super(Sale, self).write(vals)
 
 # DETAIL
 class SaleDetail(models.Model):
