11b952b557d7462b5de9fa3eba3fe79790f85e8c
Jacky supit <jsupit@gmail.com>
2024-05-16 16:46:56 +0700

    Create installasi-odoo.sh

---
+++ installasi-odoo.sh
@@ -0,0 +1,10 @@
+# install odoo 11 on ubuntu 18
+
+conda create --name odoo11-env python=3.6.13
+pip install "setuptools==58.0.0"
+sudo apt-get install libxml2-dev libxslt-dev
+
+pip install -r requirements.txt
+
+
+#ends
